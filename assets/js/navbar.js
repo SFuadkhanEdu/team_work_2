6bc8e4f0b4b9008d0c14b3862d4a52bcf6335272
Abdullayev123 <xezeraa-bp216@code.edu.az>
2024-11-21 23:02:40 +0400

    Navbar Full Commit

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,3 +8,19 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", () => {
   hiddenNav.classList.remove("show");
 });
+
+let scrollpos = window.scrollY;
+const header = document.querySelector("nav");
+const header_height = header.offsetHeight;
+const add_class_on_scroll = () => header.classList.add("scrolled");
+const remove_class_on_scroll = () => header.classList.remove("scrolled");
+
+window.addEventListener("scroll", function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= header_height) {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
+  }
+});
